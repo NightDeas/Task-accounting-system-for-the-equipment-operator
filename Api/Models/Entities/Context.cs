6a4789281e9c4bb38b0311c96139516e5468b028
NightDeas <130965236+NightDeas@users.noreply.github.com>
2024-12-03 11:56:22 +0300

    Данные ролей по умолчанию для БД

--- Api/Models/Entities/Context.cs
+++ Api/Models/Entities/Context.cs
@@ -6,15 +6,30 @@ namespace Api.Models.Entities
 {
     public class Context : IdentityDbContext<User, Role, Guid>
     {
-       
+
 
         public Context(DbContextOptions<Context> options) : base(options)
         {
-            
+
         }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            builder.Entity<Role>().HasData(new List<Role>()
+            {
+                new()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Operator"
+                },
+                   new()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Administrator"
+                },
+            });
+
+
             base.OnModelCreating(builder);
         }
 
